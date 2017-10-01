19e5dfabeaab20c56bb8bd0b2cce680c6002ed72
Harry Pan <harrypbz@outlook.com>
2017-10-01 20:07:46 +0200

    updated schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -16,27 +16,30 @@ ActiveRecord::Schema.define(version: 20171001173425) do
   enable_extension "plpgsql"
 
   create_table "settings", force: :cascade do |t|
-    t.boolean "auto_complete"
-    t.boolean "validate_address"
-    t.boolean "pobox_warning"
-    t.boolean "streetnum_warning"
+    t.boolean "auto_complete", default: true
+    t.boolean "validate_address", default: true
+    t.boolean "pobox_warning", default: false
+    t.boolean "streetnum_warning", default: false
     t.string "store_domain"
-    t.string "color_background"
-    t.string "color_border"
-    t.string "color_text"
-    t.string "color_highlight"
-    t.string "color_hover"
-    t.string "text_apt_suite"
-    t.string "text_inaccurate"
-    t.string "text_select_confirm"
-    t.string "text_select_suggestion"
+    t.string "color_background", default: "DDFFDD"
+    t.string "color_border", default: "C4E2C4"
+    t.string "color_text", default: "111111"
+    t.string "color_highlight", default: "2C5DFF"
+    t.string "color_hover", default: "FFF4BB"
+    t.string "text_apt_suite", default: "Did you specify the correct apt/suite/unit?"
+    t.string "text_inaccurate", default: "Is the shipping address correct?"
+    t.string "text_select_confirm", default: "Thanks for confirming your shipping address!"
+    t.string "text_select_suggestion", default: "Your shipping address has been updated!"
     t.string "text_suggest1"
+    t.string "Please select shipping address"
     t.string "text_suggest2"
+    t.string "To ensure prompt and accurate delivery, we suggest a modified s"
     t.string "text_correct"
+    t.string "Thanks for specifying an accurate shipping address!"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.text "text_pobox_warning"
-    t.text "text_streetnum_warning"
+    t.text "text_pobox_warning", default: "PO BOX Warning"
+    t.text "text_streetnum_warning", default: "Missing street number"
   end
 
   create_table "shops", force: :cascade do |t|
