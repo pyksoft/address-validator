b3849a2f9266057a36a43293efa7146951f74843
Harry Pan <harrypbz@outlook.com>
2017-10-01 20:01:05 +0200

    added default value

--- db/migrate/20170930170416_create_settings.rb
+++ db/migrate/20170930170416_create_settings.rb
@@ -1,23 +1,23 @@
 class CreateSettings < ActiveRecord::Migration[5.1]
   def change
     create_table :settings do |t|
-      t.boolean :auto_complete
-      t.boolean :validate_address
-      t.boolean :pobox_warning
-      t.boolean :streetnum_warning
+      t.boolean :auto_complete, default: true
+      t.boolean :validate_address, default: true
+      t.boolean :pobox_warning, default: false
+      t.boolean :streetnum_warning, default: false
       t.string :store_domain
-      t.string :color_background
-      t.string :color_border
-      t.string :color_text
-      t.string :color_highlight
-      t.string :color_hover
-      t.string :text_apt_suite
-      t.string :text_inaccurate
-      t.string :text_select_confirm
-      t.string :text_select_suggestion
-      t.string :text_suggest1
-      t.string :text_suggest2
-      t.string :text_correct
+      t.string :color_background, default: 'DDFFDD'
+      t.string :color_border, default: 'C4E2C4'
+      t.string :color_text, default: '111111'
+      t.string :color_highlight, default: '2C5DFF'
+      t.string :color_hover, default: 'FFF4BB'
+      t.string :text_apt_suite, default: 'Did you specify the correct apt/suite/unit?'
+      t.string :text_inaccurate, default: 'Is the shipping address correct?'
+      t.string :text_select_confirm, default: 'Thanks for confirming your shipping address!'
+      t.string :text_select_suggestion, default: 'Your shipping address has been updated!'
+      t.string :text_suggest1, 'Please select shipping address'
+      t.string :text_suggest2, 'To ensure prompt and accurate delivery, we suggest a modified shipping address.'
+      t.string :text_correct, 'Thanks for specifying an accurate shipping address!'
 
       t.timestamps
     end
