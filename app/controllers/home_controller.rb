fcff0d196310270f0988f6a0ae1963b03a463581
Harry Pan <harrypbz@outlook.com>
2017-10-11 18:12:32 +0200

    recurring charge

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,7 @@
 class HomeController < ShopifyApp::AuthenticatedController
   def index
+    create_recurring_application_charge
+    
     @shop = ShopifyAPI::Shop.current
     @setting = Setting.find_by store_domain: @shop.domain
     if @setting.nil?
@@ -9,4 +11,23 @@ class HomeController < ShopifyApp::AuthenticatedController
     end
     #redirect_to edit_setting_path(@setting)
   end
+
+  def create_recurring_application_charge
+    unless ShopifyAPI::RecurringApplicationCharge.current
+      recurring_application_charge = ShopifyAPI::RecurringApplicationCharge.new(
+              name: "Gift Basket Plan",
+              price: 9.99,
+              return_url: "https:\/\/#{APP_URL}\/activatecharge",
+              test: true,
+              trial_days: 7,
+              capped_amount: 100,
+              terms: "$0.99 for every order created")
+
+      if recurring_application_charge.save
+        @tokens[:confirmation_url] = recurring_application_charge.confirmation_url
+        redirect recurring_application_charge.confirmation_url
+      end
+    end
+  end
+
 end
