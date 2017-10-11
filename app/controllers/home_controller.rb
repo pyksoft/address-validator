d79d3ee90d2e734fa686705775876b0b388a194c
Harry Pan <harrypbz@outlook.com>
2017-10-11 20:15:49 +0200

    redo changes

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,7 @@
 class HomeController < ShopifyApp::AuthenticatedController
   def index
-    create_recurring_application_charge
-    
+    #create_recurring_application_charge
+
     @shop = ShopifyAPI::Shop.current
     @setting = Setting.find_by store_domain: @shop.domain
     if @setting.nil?
@@ -11,17 +11,18 @@ class HomeController < ShopifyApp::AuthenticatedController
     end
     #redirect_to edit_setting_path(@setting)
   end
-
+  
+=begin
   def create_recurring_application_charge
     unless ShopifyAPI::RecurringApplicationCharge.current
       recurring_application_charge = ShopifyAPI::RecurringApplicationCharge.new(
               name: "Gift Basket Plan",
-              price: 9.99,
-              return_url: "https:\/\/#{APP_URL}\/activatecharge",
+              price: 0.01,
+              return_url: activate_charge_url,
               test: true,
-              trial_days: 7,
+              trial_days: 30,
               capped_amount: 100,
-              terms: "$0.99 for every order created")
+              terms: "$0.01 for every order created")
 
       if recurring_application_charge.save
         @tokens[:confirmation_url] = recurring_application_charge.confirmation_url
@@ -30,4 +31,18 @@ class HomeController < ShopifyApp::AuthenticatedController
     end
   end
 
+  def activate_charge
+    recurring_application_charge = ShopifyAPI::RecurringApplicationCharge.find(request.params['charge_id'])
+    recurring_application_charge.status == "accepted" ? recurring_application_charge.activate : redirect(bulk_edit_url)
+    create_order_webhook
+    redirect root_path
+  end
+
+  def create_usage_charge
+    usage_charge = ShopifyAPI::UsageCharge.new(description: "$0.99 for every order created", price: 0.01)
+    recurring_application_charge = ShopifyAPI::RecurringApplicationCharge.current
+    usage_charge.prefix_options = {recurring_application_charge_id: recurring_application_charge.id}
+    usage_charge.save
+  end
+=end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   resources :settings, only: [:update, :edit]
   get '/settings', to: 'settings#show'
   root :to => 'home#index'
+  #get '/activate_charge', to: 'home#activate_charge'
   mount ShopifyApp::Engine, at: '/'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
