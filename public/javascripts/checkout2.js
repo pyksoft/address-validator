75b07d8a01c6429778c6d9cdc393a072e9272177
Harry Pan <harrypbz@outlook.com>
2017-10-17 16:03:50 +0200

    fix js

--- public/javascripts/checkout2.js
+++ public/javascripts/checkout2.js
@@ -130,22 +130,22 @@ var formatUnitStreet = ["Australia", "Canada", "France", "Hong Kong", "Malaysia"
           	}
           }
           xhr.send(json);*/
-          /*$.ajax({
+          $.ajax({
               type: "POST",
               url: "https://address-validation.herokuapp.com/histories",
               // The key needs to match your method's input parameter (case-sensitive).
               data: JSON.stringify({history: { shipping_address: address, country: "" }}),
               contentType: "application/json; charset=utf-8",
-              dataType: "json",
+              dataType: "jsonp",
               success: function(data){console.log(data);},
               failure: function(errMsg) {
                   console.log(errMsg);
               }
-          });*/
-          var a = JSON.stringify({history: { shipping_address: address, country: "" }});
+          });
+          /*var a = JSON.stringify({history: { shipping_address: address, country: "" }});
           $.post("https://address-validation.herokuapp.com/histories", {history: { shipping_address: address, country: "" }}, function(data) {
             //data will always be null
-          });
+          });*/
         };
 
         geocoder.geocode({'address': shipping_address}, function(results, status) {
