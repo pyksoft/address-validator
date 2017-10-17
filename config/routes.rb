2abe30f642f4758c6a3aacc1a94d1251b98bb9fd
Harry Pan <harrypbz@outlook.com>
2017-10-17 21:24:37 +0200

    fix api

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   resources :settings, only: [:update, :edit]
   get '/settings', to: 'settings#show'
   get '/histories/add', to: 'histories#add'
+  get '/histories', to: 'histories#index'
   root :to => 'home#index'
   #get '/activate_charge', to: 'home#activate_charge'
   mount ShopifyApp::Engine, at: '/'
