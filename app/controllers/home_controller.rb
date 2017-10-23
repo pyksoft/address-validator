806f218313f37bfc3ce0c4ffdb0d22f295377394
Harry Pan <harrypbz@outlook.com>
2017-10-23 17:20:46 +0200

    fix activate

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -15,25 +15,18 @@ class HomeController < ShopifyApp::AuthenticatedController
   end
 
   def create_charges
-=begin
-    application_charge = ShopifyAPI::ApplicationCharge.new({:name => "Fee for validating shipping address", :price => 0.5, :status => 'accepted', :return_url => index_charges_url})
+    application_charge = ShopifyAPI::ApplicationCharge.new({:name => "Fee for validating shipping address", :price => 0.5, :return_url => index_charges_url})
     application_charge.test = true
     #application_charge.return_url = index_charges_url
+
     if application_charge.save
-      flash[:success] = "One-time charge was successfully created."
-      #fullpage_redirect_to application_charge.confirmation_url
+      flash[:success] = "One-time charge was successfully created"
+      fullpage_redirect_to application_charge.confirmation_url
     else
       puts application_charge.errors.full_messages.first.to_s.capitalize
       flash[:danger] = application_charge.errors.full_messages.first.to_s.capitalize
       redirect_to index_charges_path
     end
-=end
-    application_charge = ShopifyAPI::ApplicationCharge.find(14123032)
-
-    #if application_charge.activate
-      flash[:success] = "One-time charge has been activated #{application_charge}"
-    #end
-    redirect_to index_charges_path
   end
 
   def activate_charges
@@ -48,8 +41,7 @@ class HomeController < ShopifyApp::AuthenticatedController
     def application_charge_params
       params.require(:application_charge).permit(
         :name,
-        :price,
-        :status
+        :price
       )
     end
 
