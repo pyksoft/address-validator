95b8602816c7063c7c3d9b2415d224eb56af5121
Harry Pan <harrypbz@outlook.com>
2017-10-23 16:56:27 +0200

    fix charge activate

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -21,8 +21,7 @@ class HomeController < ShopifyApp::AuthenticatedController
     if application_charge.save
       flash[:success] = "One-time charge was successfully created"
       #fullpage_redirect_to application_charge.confirmation_url
-      params = {:id => application_charge }
-      activate_charges
+      redirect_to index_charges_path     
     else
       puts application_charge.errors.full_messages.first.to_s.capitalize
       flash[:danger] = application_charge.errors.full_messages.first.to_s.capitalize
