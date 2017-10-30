c44eae49b7ef29e81b48ca065041bc66223d21a2
Harry Pan <harrypbz@outlook.com>
2017-10-30 10:57:44 +0100

    fix recurring

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -48,7 +48,7 @@ class HomeController < ShopifyApp::AuthenticatedController
     recurring_application_charge = ShopifyAPI::RecurringApplicationCharge.find(request.params['charge_id'])
     recurring_application_charge.status == "accepted" ? recurring_application_charge.activate : redirect(bulk_edit_url)
     #create_order_webhook
-    redirect root_path
+    redirect_to root_path
   end
 
   def create_usage_charge
