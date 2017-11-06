006f6e56dea09e0d6e6b1e10e5f552859a1b89c8
Harry Pan <harrypbz@outlook.com>
2017-11-06 16:04:08 +0100

    change routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,13 +2,14 @@ require 'resque-scheduler'
 require 'resque/scheduler/server'
 
 Rails.application.routes.draw do
-  #resources :histories
+  root :to => 'home#index'
+  mount ShopifyApp::Engine, at: '/'
+
   resources :settings, only: [:update, :edit]
   get '/settings', to: 'settings#show'
   get '/histories/add', to: 'histories#add'
   get '/histories', to: 'histories#index'
-
-  root :to => 'home#index'
+  #resources :histories
   #post '/activate_charges/:id', to: 'home#activate_charges', as: 'activate_charges'
   #get '/index_charges', to: 'home#index_charges'
   #get '/create_charges', to: 'home#create_charges'
@@ -19,6 +20,6 @@ Rails.application.routes.draw do
   mount Resque::Server.new, :at => '/resque'
 
   #get '/activate_charge', to: 'home#activate_charge'
-  mount ShopifyApp::Engine, at: '/'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
