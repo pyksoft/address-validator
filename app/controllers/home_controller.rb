c24fd0d888c2827c725215dc4763b52f4b62e5b1
Harry Pan <harrypbz@outlook.com>
2017-11-07 18:47:51 +0100

    improve job

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -61,11 +61,13 @@ class HomeController < ShopifyApp::AuthenticatedController
   end
 
   def self.daily_usage
-    Resque.logger.info "-------#{params[:price]}----------------"
+    puts "-----create usage----"
+    create_usage_charge(10)
+    #Resque.logger.info "-------#{params[:price]}----------------"
     #create_usage_charge(price: params[:price])
     #daily_usage = DailyUsage.new(price: params[:price])
-    daily_usage = DailyUsage.new(price: 10)
-    daily_usage.save
+    #daily_usage = DailyUsage.new(price: 10)
+    #daily_usage.save
   end
 
   private
--- lib/jobs/daily_usage_job.rb
+++ lib/jobs/daily_usage_job.rb
@@ -3,12 +3,6 @@ class DailyUsageJob
 
   def self.perform()
     histories = History.where(created_at: (Time.now.midnight - 1.day)..Time.now.midnight)
-    #HomeController.daily_usage(price: histories.count * 0.03)
-    #daily_usage = DailyUsage.new(price: 10)
-    #daily_usage.save
-    @usage_charge = ShopifyAPI::UsageCharge.new(description: "$0.03 for validating an shipping address", price: params[:price])
-    recurring_application_charge = ShopifyAPI::RecurringApplicationCharge.current
-    @usage_charge.prefix_options = {recurring_application_charge_id: recurring_application_charge.id}
-    @usage_charge.save
+    HomeController.delay.daily_usage(price: histories.count * 0.03)
   end
 end
