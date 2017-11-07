cae8a6d7a4279fe5701dece14afd93c5914bd87d
Harry Pan <harrypbz@outlook.com>
2017-11-07 17:05:16 +0100

    update schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171107103449) do
+ActiveRecord::Schema.define(version: 20171107154151) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "daily_usages", force: :cascade do |t|
+    t.float "price"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "histories", force: :cascade do |t|
     t.string "country"
