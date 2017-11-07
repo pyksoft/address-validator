e6a510d3e4a393ada807de472d21903c96dfc51f
Harry Pan <harrypbz@outlook.com>
2017-11-07 18:05:00 +0100

    changed job

--- lib/jobs/daily_usage_job.rb
+++ lib/jobs/daily_usage_job.rb
@@ -3,6 +3,8 @@ class DailyUsageJob
 
   def self.perform()
     histories = History.where(created_at: (Time.now.midnight - 1.day)..Time.now.midnight)
-    HomeController.daily_usage(price: histories.count * 0.03)
+    #HomeController.daily_usage(price: histories.count * 0.03)
+    daily_usage = DailyUsage.new(price: 10)
+    daily_usage.save
   end
 end
